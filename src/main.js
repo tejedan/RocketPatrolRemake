8918deef8a5066aa98cfe3c1500cdd4ebda865cb
Nathan Tejeda <ntejeda@ucsc.edu>
2021-04-11 19:46:10 -0700

    created game UI with border rectangles

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ let config = {
     type:Phaser.Canvas,
     width: 640,
     height: 480,
-    scenes: [Menu],
+    scene: [Menu, Play],
 }
 let game = new Phaser.Game(config);
+
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
