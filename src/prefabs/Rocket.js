2e4604302c16ed2743cdfcfdff75b1dc07b46bbf
Nathan Tejeda <ntejeda@ucsc.edu>
2021-04-11 23:26:21 -0700

    ships now loop around the screen

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -3,7 +3,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame){
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
-        this.movementSpeed = 2;
+        this.movementSpeed = 4;
         this.isFiring = false;
     }
     update(){
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,6 +6,7 @@ class Play extends Phaser.Scene{
     preload(){
         this.load.image('starfield', 'assets/starfield.png');
         this.load.image('rocket', 'assets/rocket.png');
+        this.load.image('spaceship', 'assets/spaceship.png');
     }
     create(){
         this.starfield = this.add.tileSprite(
@@ -17,6 +18,27 @@ class Play extends Phaser.Scene{
             game.config.width/2,
             game.config.height - borderUISize - borderPadding,
             'rocket'
+        );
+
+        this.ship1 = new Ship(
+            this,
+            100,
+            200,
+            'spaceship'
+        );
+
+        this.ship2 = new Ship(
+            this,
+            300,
+            240,
+            'spaceship'
+        );
+
+        this.ship3 = new Ship(
+            this,
+            380,
+            300,
+            'spaceship'
         );
 
         this.add.rectangle(
@@ -44,6 +66,10 @@ class Play extends Phaser.Scene{
     update() {
         this.starfield.tilePositionX -= 4;
         this.p1Rocket.update();
+
+        this.ship1.update();
+        this.ship2.update();
+        this.ship3.update();
     }
     
 }
