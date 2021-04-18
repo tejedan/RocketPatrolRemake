a470b57462a7ebd9064c3ab5a48cda2087db857f
Nathan Tejeda <ntejeda@ucsc.edu>
2021-04-17 21:21:39 -0700

    Added end condition and the ability to replay

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -80,26 +80,36 @@ class Play extends Phaser.Scene{
             fixedWidth: 100
           }
           this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
-          
+
+          this.gameOver = false;
           scoreConfig.fixedWidth = 0;
           this.clock = this.time.delayedCall(60000, () => {
               this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
               this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
+              this.gameOver = true;
           }, null, this);
 
     }
     update() {
         this.starfield.tilePositionX -= 4;
-        this.p1Rocket.update();
 
-        this.ship1.update();
-        this.ship2.update();
-        this.ship3.update();
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
+            this.scene.restart();
+        }
+
+        if (!this.gameOver) {               
+            this.p1Rocket.update();         // update rocket sprite
+            this.ship1.update();           // update spaceships (x3)
+            this.ship2.update();
+            this.ship3.update();
+        } 
+        
 
         this.checkCollision(this.p1Rocket, this.ship1);
         this.checkCollision(this.p1Rocket, this.ship2);
         this.checkCollision(this.p1Rocket, this.ship3);
-        
+
+
 
         
     }
